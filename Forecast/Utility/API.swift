e859c68dfd11eff35a1b82aa74ea7f2cf8b83409
Mansa Pratap Singh <Mansa Pratap Singh>
2021-06-06 20:19:45 +0530

    One Minor Update

--- Forecast/Utility/API.swift
+++ Forecast/Utility/API.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct API {
-    static let key = "6b0f9abfd032ccca32d2eba6fab78445"
+    static let key = "YourAPIKey"
     
     static func checkForAPIKey() {
         precondition(API.key != "YourAPIKey", "Condition: \nEither your APIKey is invalid or you haven't filled it yet. \nPlease Fill Your APIKey")
